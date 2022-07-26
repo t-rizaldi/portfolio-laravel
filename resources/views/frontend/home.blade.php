68268b5cc1bf0eb9e19eef005ee73e7a94e20e5c
t-rizaldi <t.rizaldifadli2001@gmail.com>
2022-07-26 17:20:25 +0700

    add work process

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -13,17 +13,17 @@
                         I'm a <span class="text-warning">Junior Web Developer</span> with a strong focus on problem solving and innovation.
                     </h1>
     
-                    <p class="banner-description-support">
+                    <p class="banner-description-support text-lg-start text-center">
                         Build your website with us
                     </p>
     
-                    <div class="cta">
+                    <div class="cta text-lg-start text-center">
                         <a href="#" class="btn btn-contact btn-primary">Contact Us</a>
                         <p>*Build your project with us today!</p>
                     </div>
                 </div>
-                <div class="col-lg-6">
-                    <img src="{{ asset('img/homeImage.png') }}" alt="Banner Image" class="img-fluid">
+                <div class="col-lg-6 text-center">
+                    <img src="{{ asset('img/homeImage.png') }}" alt="Banner Image" class="img-fluid mt-lg-0 mt-5">
                 </div>
             </div>
         </div>
@@ -38,8 +38,51 @@
             <div class="section-title text-center">
                 <h2>Work Process</h2>
             </div>
+
+            <div class="row justify-content-center">
+                <div class="col-lg-4 text-center">
+                    <div class="work-process">
+                        <div class="work-item">
+                            <img src="{{ asset('img/work-plan.png') }}" alt="" class="img-fluid">
+                        </div>
+                        <div class="work-process-title">
+                            <h3 class="title-work">1. Planning</h3>
+                            <p class="text-uppercase">Perencanaan Website</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 text-center">
+                    <div class="work-process">
+                        <div class="work-item">
+                            <img src="{{ asset('img/work-development.jpg') }}" alt="" class="img-fluid">
+                        </div>
+                        <div class="work-process-title">
+                            <h3 class="title-work">2. Development</h3>
+                            <p class="text-uppercase">Pengembangan Website</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 text-center">
+                    <div class="work-process">
+                        <div class="work-item">
+                            <img src="{{ asset('img/work-launch.png') }}" alt="" class="img-fluid">
+                        </div>
+                        <div class="work-process-title">
+                            <h3 class="title-work">3. Launching</h3>
+                            <p class="text-uppercase">Siap Pakai</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="work-line">
+            <img src="{{ asset('img/work-line.png') }}" alt="Image" class="img-fluid">
         </div>
     </section>
+
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0d6efd" fill-opacity="1" d="M0,288L48,240C96,192,192,96,288,64C384,32,480,64,576,64C672,64,768,32,864,16C960,0,1056,0,1152,26.7C1248,53,1344,107,1392,133.3L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+
 </main>
 
 @endsection
